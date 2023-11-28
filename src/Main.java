8d280873db9b68f8a6332c76cf77274f55512492
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-28 16:34:35 +0200

    Use the Document class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Printable printableRef;
+
+        Document document = new Document(10);
+
+        document.print();
+
+        printableRef = document;
+
+        printableRef.print();
+
+        //printableRef.getPages();//don't have methods that are not from the interface
     }
 }
